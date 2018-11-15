ecb6b930e22b4dbcfac51ab9c8f348616b3d358a
olehsalo <43565118+olehsalo@users.noreply.github.com>
2018-11-15 13:54:01 +0200

    Create simle.c

---
+++ simle.c
@@ -0,0 +1,14 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int m=5;
+    static long simple(int m){
+        int array[]=new int[m]
+        for(i=0;i<m;i++){
+            for(j=0;j<i;j++){
+                if(array[i]%array[j]!=0){
+                    return array[j];
+                }
+            }
+        }
+    }
+}
